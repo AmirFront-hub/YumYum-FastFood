b37489c0f1c5582977b0a592b203c7cb27adf2af
AmirFrontEnd-hub <amiralziyadi@gmail.com>
2024-12-12 15:38:34 +0100

    Cart fixes, can now see it stack

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,39 +1,68 @@
 import { cart } from "./menu.js";
 import { cartToSend } from "./menu.js";
 
-const cartTotal = document.querySelector("#cart-total")
+const cartTotal = document.querySelector("#cart-total");
+const paybtn = document.querySelector("#pay-btn");
 
-export function populateCart(cart) {
+export function populateCart(cart, cartToSend) {
     const cartList = document.querySelector(".cart-list");
+    cartList.innerHTML = "";
+
     let total = [];
+
     cart.forEach(item => {
         const newDiv = document.createElement("div");
-		total.push(item.price)
-        newDiv.innerHTML = `
-		<div class="cart-list">
-			<span class="item-name">${item.name}</span>
-			<span class="line"></span>
-			<span class="item-price">${item.price}</span>
-		</div> `
+        total.push(item.price * item.quantity);
 
-		const plusButton = document.createElement("button");
-		const minusButton = document.createElement("button");
-		const quantity = document.createElement("p");
-		const itemContainer = document.createElement("div");
-		
-        itemContainer.classList.add("item-container");
-        quantity.classList.add("quantity");
+        newDiv.innerHTML = `
+        <div class="cart-list">
+            <span class="item-name">${item.name}</span>
+            <span class="line"></span>
+            <span class="item-price">${item.price * item.quantity} SEK</span>
+        </div>`;
+
+        const itemContainer = document.createElement("div");
+        const itemTitle = document.createElement("h3");
+        const itemPrice = document.createElement("h3");
+        const quantityContainer = document.createElement("div");
+        const plusButton = document.createElement("button");
+        const minusButton = document.createElement("button");
+        const quantity = document.createElement("p");
+
+        quantityContainer.classList.add("quantity-container");
         plusButton.classList.add("quantity-buttons");
         minusButton.classList.add("quantity-buttons");
+        quantity.classList.add("quantity");
 
-		plusButton.innerText = "+";
-		minusButton.innerText = "-";
+        plusButton.innerText = "+";
+        minusButton.innerText = "-";
+        quantity.innerText = `${item.quantity} stycken`;
 
-		plusButton.addEventListener("click", () => plusItem);
-		minusButton.addEventListener("click", () => removeItem);
-        newDiv.append(minusButton);
-        newDiv.append(plusButton);
-        
-    cartList.append(newDiv)
-    cartTotal.innerText=total.reduce((a,b) => a + b, 0) + " SEK";
-    })}
+        plusButton.addEventListener("click", () => {
+            item.quantity += 1;
+            updateCartDisplay();
+        });
+
+        minusButton.addEventListener("click", () => {
+            if (item.quantity > 1) {
+                item.quantity -= 1;
+            } else {
+                const index = cart.indexOf(item);
+                if (index > -1) cart.splice(index, 1);
+            }
+            updateCartDisplay();
+        });
+
+        quantityContainer.append(minusButton, quantity, plusButton);
+        newDiv.append(quantityContainer);
+        cartList.append(newDiv);
+    });
+
+    cartTotal.innerText = total.reduce((a, b) => a + b, 0) + " SEK";
+}
+
+
+function updateCartDisplay() {
+    document.querySelector(".cart-list").innerHTML = "";
+    populateCart(cart, cartToSend); 
+}
--- scripts/menu.js
+++ scripts/menu.js
@@ -3,34 +3,39 @@ import { populateCart } from "./cart.js";
 const apiUrl = "https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/";
 const menuElement = document.getElementById('menu-items');
 
-let cart=[]
+let cart = [];
 let cartToSend = [];
 
 async function fetchMenuData() {
     const options = {
         method: "GET",
-        headers:{
+        headers: {
             "x-zocom": "yum-4wOFSa0vV0WtlFYK"
         }
-    }
+    };
     try {
-        const response = await fetch(apiUrl + 'menu',options);
+        const response = await fetch(apiUrl + 'menu', options);
         if (!response.ok) {
-            throw new Error('could not fetch');
+            throw new Error('Could not fetch menu data');
         }
         const data = await response.json();
-        return data
+        return data;
 
-    } catch (error){
-        console.error("error fetching menu data", error)
+    } catch (error) {
+        console.error("Error fetching menu data:", error);
     }
 }
 
-function addToCart(item){
-    cart.push(item)
-    cartToSend.push(item.id)
-    console.log(cart)
-    populateCart(cart)
+function addToCart(item) {
+    const existingItem = cart.find(cartItem => cartItem.id === item.id);
+    if (existingItem) {
+        existingItem.quantity += 1;
+    } else {
+        cart.push({ ...item, quantity: 1 });
+        cartToSend.push(item.id);
+    }
+    console.log(cart);
+    populateCart(cart, cartToSend);
 }
 
 function populateMenu(menuData) {
@@ -45,48 +50,49 @@ function populateMenu(menuData) {
     wontonList.forEach(wonton => {
         const newDiv = document.createElement("div");
         newDiv.innerHTML = `
-		<div class="w-item">
-			<span class="item-name">${wonton.name}</span>
-			<span class="line"></span>
-			<span class="item-price">${wonton.price}</span> <br>
-			<span class="item-details">${wonton.ingredients}</span>
-		</div> `
+        <div class="w-item">
+            <span class="item-name">${wonton.name}</span>
+            <span class="line"></span>
+            <span class="item-price">${wonton.price} SEK</span><br>
+            <span class="item-details">${wonton.ingredients}</span>
+        </div>`;
 
-		newDiv.addEventListener("click", () => {
+        newDiv.addEventListener("click", () => {
             addToCart(wonton);
         });
 
-    wontonsMenu.append(newDiv)
-    })
+        wontonsMenu.append(newDiv);
+    });
 
-    dipsList.forEach(
-        dip => {
-            const newDiv2 = document.createElement("div");
-            newDiv2.innerHTML =`
-            <input type="radio" name="dips">
-            <label for="">${dip.name}</label>`
-            newDiv2.classList.add("dip-btn")
+    dipsList.forEach(dip => {
+        const newDiv = document.createElement("div");
+        newDiv.innerHTML = `
+        <input type="radio" name="dips">
+        <label>${dip.name}</label>`;
+        newDiv.classList.add("dip-btn");
 
-			newDiv2.addEventListener("click", () => {
-				addToCart(dip);
-			});
+        newDiv.addEventListener("click", () => {
+            addToCart(dip);
+        });
 
-			dipsMenu.append(newDiv2);})
+        dipsMenu.append(newDiv);
+    });
 
-    drinkList.forEach(
-        drink => {
-            const newDiv3 = document.createElement("div");
-            newDiv3.innerHTML = `
-            <input type="radio" name="drinks">
-            <label for="">${drink.name}</label>`
-            newDiv3.classList.add("drink-btn")
+    drinkList.forEach(drink => {
+        const newDiv = document.createElement("div");
+        newDiv.innerHTML = `
+        <input type="radio" name="drinks">
+        <label>${drink.name}</label>`;
+        newDiv.classList.add("drink-btn");
 
-			newDiv3.addEventListener("click", () => {
-				addToCart(drink);
-			});
-	
-    	drinksMenu.append(newDiv3)})
+        newDiv.addEventListener("click", () => {
+            addToCart(drink);
+        });
+
+        drinksMenu.append(newDiv);
+    });
 }
+
 populateMenu(await fetchMenuData());
 
-export {cart, cartToSend};
+export { cart, cartToSend };
